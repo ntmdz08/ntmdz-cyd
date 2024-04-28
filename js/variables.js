c8866a7bd09ffa6639f23b75a90c718383eb77e3
sage <user1@MacBook-Air.lan>
2024-04-28 01:27:17 -0500

    RGB LED enabled for builds w/o GPS

--- js/variables.js
+++ js/variables.js
@@ -17,13 +17,13 @@ const MCYDlatestFiles = {
 const MCYDNOGPSpreviousFiles = {
     'bootloader': 'resources/STATIC/M/CYD/esp32_marauder.ino.bootloader.bin',
     'partitions': 'resources/STATIC/CYD/FLIPDEV/esp32_marauder.ino.partitions.bin',
-    'firmware': 'resources/CURRENT/esp32_marauder_v0_13_10_20240426_cyd_nogpspwn.bin',
+    'firmware': 'resources/CURRENT/esp32_marauder_v0_13_10_20240428_cyd_nogpspwn.bin',
 };
 // Latest Marauder for CYDNOGPS
 const MCYDNOGPSlatestFiles = {
     'bootloader': 'resources/STATIC/M/CYD/esp32_marauder.ino.bootloader.bin',
     'partitions': 'resources/STATIC/M/CYD/esp32_marauder.ino.partitions.bin',
-    'firmware': 'resources/CURRENT/esp32_marauder_v0_13_10_20240426_cyd_nogpspwn.bin',
+    'firmware': 'resources/CURRENT/esp32_marauder_v0_13_10_20240428_cyd_nogpspwn.bin',
 };
 
 
@@ -48,13 +48,13 @@ const MCYD2USBlatestFiles = {
 const MCYD2USBNOGPSpreviousFiles = {
     'bootloader': 'resources/STATIC/M/CYD/esp32_marauder.ino.bootloader.bin',
     'partitions': 'resources/STATIC/M/CYD/esp32_marauder.ino.partitions.bin',
-    'firmware': 'resources/CURRENT/esp32_marauder_v0_13_10_20240426_cyd2usb_nogpspwn.bin',
+    'firmware': 'resources/CURRENT/esp32_marauder_v0_13_10_20240428_cyd2usb_nogpspwn.bin',
 };
 // Latest Marauder for CYD2USBNOGPS
 const MCYD2USBNOGPSlatestFiles = {
     'bootloader': 'resources/STATIC/M/CYD/esp32_marauder.ino.bootloader.bin',
     'partitions': 'resources/STATIC/M/CYD/esp32_marauder.ino.partitions.bin',
-    'firmware': 'resources/CURRENT/esp32_marauder_v0_13_10_20240426_cyd2usb_nogpspwn.bin',
+    'firmware': 'resources/CURRENT/esp32_marauder_v0_13_10_20240428_cyd2usb_nogpspwn.bin',
 };
 
 
